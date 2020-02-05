416e4c7076a7e18a80a44688011b78f9dcdd118f
ilpeppino <ilpeppino@gmail.com>
2020-02-05 15:12:22 +0100

    build stadium and logged a goal

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Soccer Ball")
+        {
+            Debug.Log("GOAL!");
+        }
+    }
+
+}
